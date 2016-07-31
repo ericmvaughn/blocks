668ffdcf041e7a4f3c3cebedddd3ad70a3d46d6a
ericmvaughn <eric_vaughn@yahoo.com>
2016-07-31 13:58:29 -0400

    Add unit testing for the REST interface

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var assert = require('chai').assert;
+var request =require('superagent');
 var util = require('../util.js');
 var validBlock = require('./valid-block.json');
+var url = 'http://localhost:3000';
 
 describe('ProtoBuf', function() {
   describe('decodePayload', function() {
@@ -33,6 +35,244 @@ describe('ProtoBuf', function() {
       var type = util.decodeType(validBlock);
       assert.isDefined(type);
       assert.oneOf(type, ['CHAINCODE_DEPLOY', 'CHAINCODE_INVOKE', 'CHAINCODE_QUERY', 'CHAINCODE_TERMINATE']);
+    });
+  });
+});
+
+var lastBlock = 1;
+
+describe('Blockchain REST interface', function() {
+  describe('bad endpoint', function() {
+    it('should return a 404 error', function(done) {
+      request
+      .get(url + '/badEndPoint')
+      .end(function(err, res) {
+        assert.isNotNull(err);
+        //console.log(err.response.error);
+        assert.equal(err.status, 404, 'status should be 404');
+        done();
+      })
+    });
+  });
+  describe('GET /chain', function() {
+    it('should return valid data', function(done) {
+      request
+      .get(url + '/chain')
+      .end(function(err, res) {
+        assert.isNull(err);
+        //console.log(res.body);
+        assert.equal(res.status, 200, 'should recieve a 200 status');
+        assert.isObject(res.body, 'should return chain stats');
+        assert.property(res.body, 'height', 'chain stats should contain height');
+        assert.isNumber(res.body.height, 'height should be a number');
+        lastBlock = res.body.height - 1;
+        assert.property(res.body, 'currentBlockHash',
+                                'chain stats should contain currentBlockHash');
+        assert.isString(res.body.currentBlockHash, 'hash should be a string');
+        assert.property(res.body, 'previousBlockHash',
+                                'chain stats should contain previousBlockHash');
+        assert.isString(res.body.previousBlockHash, 'hash should be a string');
+        done();
+      });
+    });
+  });
+  describe('GET lastest block', function() {
+    it('should return valid block at the end of the chain', function(done) {
+      request
+      .get(url + '/chain/blocks/' + lastBlock)
+      .end(function(err, res) {
+        assert.isNull(err);
+        //console.log(res.body.transactions[0].payload);
+        assert.equal(res.status, 200, 'should recieve a 200 status');
+        assert.isObject(res.body, 'should return a block');
+        assert.property(res.body, 'transactions', 'the block should contain transactions');
+        assert.property(res.body.transactions[0], 'payload',
+                'the transaction should have a payload');
+        assert.property(res.body.transactions[0].payload, 'chaincodeSpec',
+                'the payload should have a chaincodeSpec');
+        done();
+      });
+    });
+  });
+  describe('GET payload by block ID', function() {
+    it('should return valid payload for the last block of the chain', function(done) {
+      request
+      .get(url + '/payload/' + lastBlock)
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200, 'should recieve a 200 status');
+        assert.isObject(res.body, 'should return a payload');
+        //console.log(res.body);
+        assert.property(res.body, 'chaincodeSpec',
+          'the payload should have a chaincodeSpec');
+        done();
+      });
+    });
+  });
+  describe('GET a block list', function() {
+    it('should return a list of 4 blocks from the end of the chain', function(done) {
+      request
+      .get(url + '/chain/blockList/' + 4)
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200, 'should recieve a 200 status');
+        assert.isArray(res.body, 'should return list of blocks');
+        //console.log(res.body);
+        assert.property(res.body[0], 'id', 'a memeber of the array should contain an id');
+        assert.equal(res.body[0].id, lastBlock,
+          'the id of the 1st member should be the last block');
+        assert.lengthOf(res.body, 4, 'there should be 4 blocks in the list');
+        assert.property(res.body[0], 'block',
+          'a memeber of the array should contain a block');
+        assert.property(res.body[0].block, 'transactions',
+          'a block should have transactions');
+        done();
+      });
+    });
+  });
+  describe('GET a transaction list', function() {
+    it('should return a list of 4 transaction', function(done) {
+      request
+      .get(url + '/chain/transactionList/' + 4)
+      .end(function(err, res) {
+        assert.isNull(err);
+        assert.equal(res.status, 200, 'should recieve a 200 status');
+        assert.isArray(res.body, 'should return list of transactions');
+        //console.log(res.body);
+        assert.property(res.body[0], 'id',
+          'a memeber of the array should contain an id');
+        assert.lengthOf(res.body, 4, 'there should be 4 transactions in the list');
+        assert.property(res.body[0], 'transaction',
+          'a memeber of the array should contain a transaction');
+        assert.property(res.body[0].transaction, 'payload',
+          'a transaction should have a payload');
+        done();
+      });
+    });
+  });
+});
+
+//testing the REST interface that uses the node SDK
+describe('Chaincode REST interface', function() {
+//   before(function(done) {
+//     request
+//     .post(url + '/addUser')
+//     .send({name: 'testUser3', amount: '100'})
+//     .end(function(err, res) {});
+//     request
+//     .post(url + '/addUser')
+//     .send({name: 'testUser4', amount: '200'})
+//     .end(function(err, res) {});
+//     setTimeout(function() {
+//       done();
+//     }, 2000);    // hack to give time for the transaction to complete
+// });
+
+  describe('/addUser', function() {
+    it('should add a new user', function(done) {
+      request
+      .post(url + '/addUser')
+      .send({name: 'testUser1', amount: '100'})
+      .end(function(err, res) {
+      assert.isNull(err);
+      //console.log(res.body);
+      assert.property(res.body, 'uuid', 'reply should have uuid');
+      done();
+      });
+    });
+    it('should add a second user', function(done) {
+      request
+      .post(url + '/addUser')
+      .send({name: 'testUser2', amount: '200'})
+      .end(function(err, res) {
+      assert.isNull(err);
+      //console.log(res.body);
+      assert.property(res.body, 'uuid', 'reply should have uuid');
+      setTimeout(function() {
+        done();
+      }, 1000);    // hack to give time for the transaction to complete
+      });
+    });
+  });
+  describe('/userList', function(){
+    it('should return a list of users', function(done) {
+      request
+      .get(url + '/userList')
+      .accept('application/json')
+      .end(function(err, res) {
+        assert.isNull(err);
+        //console.log(res.body);
+        assert.property(res.body, 'testUser1', 'testUser1 should be in the list');
+        assert.equal(res.body.testUser1, 100, 'testUser1 amount should be 100');
+        assert.property(res.body, 'testUser2', 'testUser2 should be in the list');
+        assert.equal(res.body.testUser2, 200, 'testUser2 amount should be 200');
+        done();
+      });
+    });
+  });
+  describe('/transfer', function() {
+    it('should transfer 10 from testUser1 to testUser2', function(done) {
+      request
+      .post(url + '/transfer')
+      .send({fromName: 'testUser1', toName: 'testUser2', amount: '10'})
+      .end(function(err, res) {
+        assert.isNull(err);
+        console.log(res.body);
+        assert.property(res.body, 'uuid', 'reply should have uuid');
+        setTimeout(function() {
+          done();
+        }, 1000);    // hack to give time for the transaction to complete
+      });
+    });
+    it('should transfer correct amount', function(done) {
+      request
+      .get(url + '/userList')
+      .end(function(err, res) {
+        assert.isNull(err);
+        //console.log(res.body);
+        assert.property(res.body, 'testUser1', 'testUser1 should be in the list');
+        assert.equal(res.body.testUser1, 90, 'testUser1 amount should be 90');
+        assert.property(res.body, 'testUser2', 'testUser2 should be in the list');
+        assert.equal(res.body.testUser2, 210, 'testUser2 amount should be 210');
+        done();
+      });
+    });
+  });
+  describe('/delUser', function() {
+    it('should delete a user', function(done) {
+      request
+      .post(url + '/delUser')
+      .send({name: 'testUser1'})
+      .end(function(err, res) {
+      assert.isNull(err);
+      //console.log(res.body);
+      assert.property(res.body, 'uuid', 'reply should have uuid');
+      done();
+      });
+    });
+    it('should delete second test user', function(done) {
+      request
+      .post(url + '/delUser')
+      .send({name: 'testUser2'})
+      .end(function(err, res) {
+      assert.isNull(err);
+      //console.log(res.body);
+      assert.property(res.body, 'uuid', 'reply should have uuid');
+      setTimeout(function() {
+        done();
+      }, 1000);    // hack to give time for the transaction to complete
+      });
+    });
+    it('both test users should be gone', function(done) {
+      request
+      .get(url + '/userList')
+      .end(function(err, res) {
+        assert.isNull(err);
+        //console.log(res.body);
+        assert.notProperty(res.body, 'testUser1', 'testUser1 should not be in the list');
+        assert.notProperty(res.body, 'testUser2', 'testUser2 should be not in the list');
+        done();
+      });
     });
   });
 });
